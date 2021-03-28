e38230db9864c5aad53c660390c967c0a5997c38
Kpojluku <Anton.kpojluku@gmail.com>
2021-03-28 13:02:58 +0300

    issue 3

--- src/main/java/com/goltsov/controller/FileUploadController.java
+++ src/main/java/com/goltsov/controller/FileUploadController.java
@@ -6,6 +6,7 @@ import com.goltsov.model.JsonFile;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -16,22 +17,28 @@ public class FileUploadController {
     @GetMapping("/upload")
     public String listUploadedFiles(Model model) {
         model.addAttribute("upload");
+
         return "upload";
     }
 
     @PostMapping("/upload")
-    public String handleFileUpload(@RequestParam("file") MultipartFile[] file) {
+    public String handleFileUpload(@RequestParam("file") MultipartFile[] file,
+                                   @ModelAttribute("jsonfile1") JsonFile jsonfile1,
+                                   @ModelAttribute("jsonfile2") JsonFile jsonfile2) {
         JsonComparator jsonComparator = new JsonComparator();
         try {
-            JsonFile jsonFile1 = jsonComparator.getJsonFile(file[0]);
-            JsonFile jsonFile2 = jsonComparator.getJsonFile(file[1]);
-            System.out.println(jsonFile1);
+            JsonFile File1 = jsonComparator.getJsonFile(file[0]);
+            JsonFile File2 = jsonComparator.getJsonFile(file[1]);
+          //  jsonfile1.setMetadata(File1.getMetadata());
+         //   jsonfile2.setMetadata(File2.getMetadata());
+            jsonComparator.setJsonFile(File1, jsonfile1);
+            jsonComparator.setJsonFile(File2, jsonfile2);
+
+           return jsonComparator.compare(jsonfile1, jsonfile2);
 
         }catch (Exception e){
             e.printStackTrace();
             return "error";
         }
-
-        return "upload";
     }
 }
--- src/main/java/com/goltsov/model/JsonComparator.java
+++ src/main/java/com/goltsov/model/JsonComparator.java
@@ -10,20 +10,36 @@ import java.nio.charset.StandardCharsets;
 
 public class JsonComparator {
 
-public JsonFile getJsonFile(MultipartFile file)throws IOException {
-    String content = new String(file.getBytes(), StandardCharsets.UTF_8);
-    return getJsonFile(content);
-}
-    public JsonFile getJsonFile(String json){
-    JsonFile jsonFile = new JsonFile();
-    ObjectMapper mapper = new ObjectMapper();
-    mapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
-        try {
-            jsonFile = mapper.readValue(json, JsonFile.class);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
+    public JsonFile getJsonFile(MultipartFile file) throws IOException {
+        String content = new String(file.getBytes(), StandardCharsets.UTF_8);
+        return getJsonFile(content);
+    }
+
+    public JsonFile getJsonFile(String json) throws JsonProcessingException {
+        JsonFile jsonFile;
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
+        jsonFile = mapper.readValue(json, JsonFile.class);
 
         return jsonFile;
+    }
+    public void setJsonFile(JsonFile jsonFile, JsonFile finalFile)  {
+       finalFile.setMetadata(jsonFile.getMetadata());
+       finalFile.setServices(jsonFile.getServices());
+       finalFile.setArtifacts(jsonFile.getArtifacts());
+       finalFile.setScript(jsonFile.getScript());
+       finalFile.setRpm(jsonFile.getRpm());
+       finalFile.setParameters(jsonFile.getParameters());
+    }
+
+    public String compare(JsonFile jsonFile1, JsonFile jsonFile2) {
+        //Перед сравнением проверить на наличие все обязательные объекты
+
+       // System.out.println(jsonFile1.getMetadata().getDescription().getVersion()==
+      //          jsonFile2.getMetadata().getDescription().getVersion());
+
+
+
+        return "report";
     }
 }
