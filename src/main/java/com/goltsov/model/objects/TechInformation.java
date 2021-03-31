9de1f6459cf0830655306bad98982c241011976f
Kpojluku <Anton.kpojluku@gmail.com>
2021-03-31 23:23:39 +0300

    I started to implement the comparison algorithm

---
+++ src/main/java/com/goltsov/model/objects/TechInformation.java
@@ -0,0 +1,13 @@
+package com.goltsov.model.objects;
+
+public class TechInformation {
+    private int servicesLength;
+
+    public int getServicesLength() {
+        return servicesLength;
+    }
+
+    public void setServicesLength(int servicesLength) {
+        this.servicesLength = servicesLength;
+    }
+}
